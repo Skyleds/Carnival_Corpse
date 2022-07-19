218069099439ae41e598528a3614d2c06cac6ac0
Skyleds <leonardodrondonv@gmail.com>
2022-07-19 17:41:10 -0400

    Agregada la funcion de moverse la animacion al caminar hacia abajo

---
+++ copy.bat
@@ -0,0 +1,3 @@
+copy  "D:\Biblioteca\Documentos\Unity\Carnival Corpse\Assets\Scripts\GameManager.cs"  "C:\Users\Leonar~1\GIT\Carnival_Corpse\GameManager.cs"
+copy  "D:\Biblioteca\Documentos\Unity\Carnival Corpse\Assets\Scripts\Player.cs"  "C:\Users\Leonar~1\GIT\Carnival_Corpse\Player.cs"
+pause
