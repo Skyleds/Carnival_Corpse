bf03e7f3f20f9146ead706a84d9706d1dcb3ea84
Skyleds <leonardodrondonv@gmail.com>
2022-07-22 16:07:42 -0400

    Agregado script Enemies

--- copy.bat
+++ copy.bat
@@ -1,4 +1,5 @@
 copy  "D:\Biblioteca\Documentos\Unity\Carnival Corpse\Assets\Scripts\GameManager.cs"  "C:\Users\Leonar~1\GIT\Carnival_Corpse\GameManager.cs"
 copy  "D:\Biblioteca\Documentos\Unity\Carnival Corpse\Assets\Scripts\CameraFollow.cs"  "C:\Users\Leonar~1\GIT\Carnival_Corpse\CameraFollow.cs"
 copy  "D:\Biblioteca\Documentos\Unity\Carnival Corpse\Assets\Scripts\Player.cs"  "C:\Users\Leonar~1\GIT\Carnival_Corpse\Player.cs"
+copy  "D:\Biblioteca\Documentos\Unity\Carnival Corpse\Assets\Scripts\Enemies.cs"  "C:\Users\Leonar~1\GIT\Carnival_Corpse\Enemies.cs"
 pause
