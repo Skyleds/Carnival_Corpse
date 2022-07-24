7b3828fdf3ae0e1949f8b16f494b703ed1f6651f
Skyleds <leonardodrondonv@gmail.com>
2022-07-23 23:51:01 -0400

    Añadido metodo para hacer daño dependiendo del collider colisionado

--- Enemies.cs
+++ Enemies.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Enemies : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+
+    void OnTriggerEnter2D(Collider2D player) {
+        if (player.tag == "Player") {
+            if (player.GetType () == typeof(BoxCollider2D)){
+                Player.sharedInstance.DamageTaken (5); 
+            } else if (player.GetType () == typeof(CircleCollider2D)){
+                Player.sharedInstance.DamageTaken (10); 
+            }
+            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
