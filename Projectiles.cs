2929d72765fdbb94693d364badb90b1986dc4be6
Skyleds <leonardodrondonv@gmail.com>
2022-07-25 15:21:34 -0400

    Nuevo script para manejar los proyectiles

---
+++ Projectiles.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectiles : MonoBehaviour
+{
+
+    float arrowSpeed = 1.0f;
+
+    void ShootArrow (){
+        
+    }
+
+
+
+
+}
